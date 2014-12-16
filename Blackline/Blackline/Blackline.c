59f6b87cde02b409b0970343dfed7adc0d2e3fc9
asabeeh18 <asabeeh18@hotmail.com>
2014-12-16 16:39:08 +0530

    ANgle rot Jig

--- Blackline/Blackline/Blackline.c
+++ Blackline/Blackline/Blackline.c
@@ -16,11 +16,11 @@ unsigned char flag = 0;
 unsigned char Left_white_line = 0;
 unsigned char Center_white_line = 0;
 unsigned char Right_white_line = 0;
-unsigned char Sharp_Sensor = 0; 
-
+//
 unsigned long int ShaftCountLeft = 0; //to keep track of left position encoder
 unsigned long int ShaftCountRight = 0; //to keep track of right position encoder
 unsigned int Degrees; //to accept angle in degrees for turning
+
 
 //Function to configure LCD port
 void lcd_port_config (void)
@@ -46,6 +46,21 @@ void motion_pin_config (void)
  DDRL = DDRL | 0x18;   //Setting PL3 and PL4 pins as output for PWM generation
  PORTL = PORTL | 0x18; //PL3 and PL4 pins are for velocity control using PWM.
 }
+//
+
+//Function to configure INT4 (PORTE 4) pin as input for the left position encoder
+void left_encoder_pin_config (void)
+{
+	DDRE  = DDRE & 0xEF;  //Set the direction of the PORTE 4 pin as input
+	PORTE = PORTE | 0x10; //Enable internal pull-up for PORTE 4 pin
+}
+
+//Function to configure INT5 (PORTE 5) pin as input for the right position encoder
+void right_encoder_pin_config (void)
+{
+	DDRE  = DDRE & 0xDF;  //Set the direction of the PORTE 4 pin as input
+	PORTE = PORTE | 0x20; //Enable internal pull-up for PORTE 4 pin
+}
 
 //Function to Initialize PORTS
 void port_init()
@@ -53,6 +68,10 @@ void port_init()
 	lcd_port_config();
 	adc_pin_config();
 	motion_pin_config();	
+	//
+	left_encoder_pin_config(); //left encoder pin config
+	right_encoder_pin_config(); //right encoder pin config
+
 }
 
 // Timer 5 initialized in PWM mode for velocity control
@@ -118,23 +137,58 @@ void velocity (unsigned char left_motor, unsigned char right_motor)
 	OCR5AL = (unsigned char)left_motor;
 	OCR5BL = (unsigned char)right_motor;
 }
-
-
-//sdsdjskd=========
-void motion_set (unsigned char Direction)
+//
+void left_position_encoder_interrupt_init (void) //Interrupt 4 enable
 {
-	unsigned char PortARestore = 0;
-
-	Direction &= 0x0F; 		// removing upper nibbel for the protection
-	PortARestore = PORTA; 		// reading the PORTA original status
-	PortARestore &= 0xF0; 		// making lower direction nibbel to 0
-	PortARestore |= Direction; // adding lower nibbel for forward command and restoring the PORTA status
-	PORTA = PortARestore; 		// executing the command
+	cli(); //Clears the global interrupt
+	EICRB = EICRB | 0x02; // INT4 is set to trigger with falling edge
+	EIMSK = EIMSK | 0x10; // Enable Interrupt INT4 for left position encoder
+	sei();   // Enables the global interrupt
 }
 
-void forward (void) //both wheels forward
+void right_position_encoder_interrupt_init (void) //Interrupt 5 enable
 {
-	motion_set(0x06);
+	cli(); //Clears the global interrupt
+	EICRB = EICRB | 0x08; // INT5 is set to trigger with falling edge
+	EIMSK = EIMSK | 0x20; // Enable Interrupt INT5 for right position encoder
+	sei();   // Enables the global interrupt
+}
+
+//ISR for right position encoder
+ISR(INT5_vect)
+{
+	ShaftCountRight++;  //increment right shaft position count
+}
+
+
+//ISR for left position encoder
+ISR(INT4_vect)
+{
+	ShaftCountLeft++;  //increment left shaft position count
+}
+
+
+
+//Function used for setting motor's direction
+void motion_set (unsigned char Direction)
+{
+ unsigned char PortARestore = 0;
+
+ Direction &= 0x0F; 		// removing upper nibbel for the protection
+ PortARestore = PORTA; 		// reading the PORTA original status
+ PortARestore &= 0xF0; 		// making lower direction nibbel to 0
+ PortARestore |= Direction; // adding lower nibbel for forward command and restoring the PORTA status
+ PORTA = PortARestore; 		// executing the command
+}
+
+void forward (void) 
+{
+  motion_set (0x06);
+}
+
+void stop (void)
+{
+  motion_set (0x00);
 }
 
 void back (void) //both wheels backward
@@ -173,11 +227,7 @@ void soft_right_2 (void) //Left wheel stationary, Right wheel backward
 }
 
 
-void stop (void)
-{
-	motion_set(0x00);
-}
-//angle
+//Function used for turning robot by specified degrees
 void angle_rotate(unsigned int Degrees)
 {
 	float ReqdShaftCount = 0;
@@ -191,40 +241,100 @@ void angle_rotate(unsigned int Degrees)
 	while (1)
 	{
 		if((ShaftCountRight >= ReqdShaftCountInt) | (ShaftCountLeft >= ReqdShaftCountInt))
-		break;
+		{
+			lcd_print(2,2,0,1);
+				break;
+		}
+		lcd_print(2,2,ShaftCountLeft,3);
+		lcd_print(2,5,ShaftCountRight,3);
+		
+	}
+	stop(); //Stop robot
+	
+}
+
+//Function used for moving robot forward by specified distance
+
+void linear_distance_mm(unsigned int DistanceInMM)
+{
+	float ReqdShaftCount = 0;
+	unsigned long int ReqdShaftCountInt = 0;
+
+	ReqdShaftCount = DistanceInMM / 5.338; // division by resolution to get shaft count
+	ReqdShaftCountInt = (unsigned long int) ReqdShaftCount;
+	
+	ShaftCountRight = 0;
+	while(1)
+	{
+		if(ShaftCountRight > ReqdShaftCountInt)
+		{
+			break;
+		}
 	}
 	stop(); //Stop robot
 }
 
-//ISR for right position encoder
-ISR(INT5_vect)
-{
-	ShaftCountRight++;  //increment right shaft position count
-}
-
-
-//ISR for left position encoder
-ISR(INT4_vect)
-{
-	ShaftCountLeft++;  //increment left shaft position count
-}
-
-
-
-
-
-//Function used for moving robot forward by specified distance
-
-
 void forward_mm(unsigned int DistanceInMM)
 {
 	forward();
+	linear_distance_mm(DistanceInMM);
 }
 
 void back_mm(unsigned int DistanceInMM)
 {
 	back();
+	linear_distance_mm(DistanceInMM);
 }
+
+void left_degrees(unsigned int Degrees)
+{
+	// 88 pulses for 360 degrees rotation 4.090 degrees per count
+	left(); //Turn left
+	angle_rotate(Degrees);
+}
+
+
+
+void right_degrees(unsigned int Degrees)
+{
+	// 88 pulses for 360 degrees rotation 4.090 degrees per count
+	right(); //Turn right
+	angle_rotate(Degrees);
+}
+
+
+void soft_left_degrees(unsigned int Degrees)
+{
+	// 176 pulses for 360 degrees rotation 2.045 degrees per count
+	soft_left(); //Turn soft left
+	Degrees=Degrees*2;
+	angle_rotate(Degrees);
+}
+
+void soft_right_degrees(unsigned int Degrees)
+{
+	// 176 pulses for 360 degrees rotation 2.045 degrees per count
+	soft_right();  //Turn soft right
+	Degrees=Degrees*2;
+	angle_rotate(Degrees);
+}
+
+void soft_left_2_degrees(unsigned int Degrees)
+{
+	// 176 pulses for 360 degrees rotation 2.045 degrees per count
+	soft_left_2(); //Turn reverse soft left
+	Degrees=Degrees*2;
+	angle_rotate(Degrees);
+}
+
+void soft_right_2_degrees(unsigned int Degrees)
+{
+	// 176 pulses for 360 degrees rotation 2.045 degrees per count
+	soft_right_2();  //Turn reverse soft right
+	Degrees=Degrees*2;
+	angle_rotate(Degrees);
+}
+
 
 void buzzer_on (void)
 {
@@ -242,159 +352,74 @@ void buzzer_off (void)
 	PORTC = port_restore;
 }
 
+
 void init_devices (void)
 {
  	cli(); //Clears the global interrupts
-	port_init();	
-	
+	port_init();
+	adc_init();
+	left_position_encoder_interrupt_init();
+	right_position_encoder_interrupt_init();
 	DDRC = DDRC | 0x08;		//Setting PORTC 3 as output
 	PORTC = PORTC & 0xF7;
-	adc_init();
 	timer5_init();
-	//SERVO
-	TCCR1B = 0x00; //stop
-	TCNT1H = 0xFC; //Counter high value to which OCR1xH value is to be compared with
-	TCNT1L = 0x01;	//Counter low value to which OCR1xH value is to be compared with
-	ICR1H  = 0x03;	
-	ICR1L  = 0xFF;
-	TCCR1A = 0xAB; /*{COM1A1=1, COM1A0=0; COM1B1=1, COM1B0=0; COM1C1=1 COM1C0=0}
- 						For Overriding normal port functionality to OCRnA outputs.
-					  {WGM11=1, WGM10=1} Along With WGM12 in TCCR1B for Selecting FAST PWM Mode*/
-	TCCR1C = 0x00;
-	TCCR1B = 0x0C; //WGM12=1; CS12=1, CS11=0, CS10=0 (Prescaler=256)
-
-	//SERVO1
-	DDRB  = DDRB | 0x20;  //making PORTB 5 pin output
-	PORTB = PORTB | 0x20; //setting PORTB 5 pin to logic 1
-	OCR1AH = 0x03;	//Output compare Register high value for servo 1
-	OCR1AL = 0xFF;	//Output Compare Register low Value For servo 1
-
 	sei();   //Enables the global interrupts
-	
 }
-void set_sensor()
+
+
+// forward jaa plz
+
+void set_color()
 {
 	Left_white_line = ADC_Conversion(3);	//Getting data of Left WL Sensor
 	Center_white_line = ADC_Conversion(2);	//Getting data of Center WL Sensor
 	Right_white_line = ADC_Conversion(1);	//Getting data of Right WL Sensor
-	Sharp_Sensor=ADC_Conversion(11);
-	
-}
-void correct(unsigned int go)
-{
-	lcd_print(1,2,go,4);
-	motion_set(go);
-	if(go==0x02)
-		velocity(100,150);
-	else
-		velocity(150,100);
-	forward();
-	_delay_ms(10000);
-	stop();
-	buzzer_off();
-}
 
-void gadbad()
+}
+void noNatak()
 {
-	lcd_print(1,1,3,1);
-	correct(0x04); //soft left
-	set_sensor();
-	if(Center_white_line>=40 && Left_white_line<40 && Right_white_line<40)
-		return;
 	buzzer_on();
-	correct(0x02); //soft right
-	correct(0x02); //soft right
+	Degrees=4;
+	left(); //Left wheel backward, Right wheel forward
+	angle_rotate(Degrees);
+	
+	lcd_print(2,1,7,1);
+	_delay_ms(1000);
+	stop();
+	lcd_print(2,1,6,1);
+	buzzer_off();
+	return;
 }
 
-void servo_1(unsigned char degrees)
+
+void forwardJaa()
 {
-	float PositionPanServo = 0;
-	PositionPanServo = ((float)degrees / 1.86) + 35.0;
-	OCR1AH = 0x00;
-	OCR1AL = (unsigned char) PositionPanServo;
-}
-void pickup()
-{
+	do
+	{
+		set_color();
+		print_sensor(1,1,3);	//Prints value of White Line Sensor1
+		print_sensor(1,5,2);	//Prints Value of White Line Sensor2
+		print_sensor(1,9,1);	//Prints Value of White Line Sensor3
 	
-	servo_1(0);
-	_delay_ms(2000);
-	servo_1(90);
-	_delay_ms(8000);
-	lcd_print(1,8,11,2);
-	servo_1(0);
+		forward();
+		velocity(150,150);
+	
+	}
+	while(Center_white_line>0x28);
+	
+	noNatak();	
+	return;
 }
 
 //Main Function
 int main()
 {
-	int f1=0;
 	init_devices();
 	lcd_set_4bit();
 	lcd_init();
-	
+	forwardJaa();	
 	while(1)
 	{
-		set_sensor();
-		flag=0;
-		print_sensor(1,4,11);   //SHARP sensor
-		print_sensor(2,1,3);	//Prints value of White Line Sensor1
-		print_sensor(2,5,2);	//Prints Value of White Line Sensor2
-		print_sensor(2,9,1);	//Prints Value of White Line Sensor3
-		
-		//good to go do dis even if ALL WHITE  !!
-		if(Center_white_line>40)
-		{
-			lcd_print (1,1,0,1);
-			flag=1;
-			forward();
-			velocity(150,150);
-			f1=0;
-		}
-		
-		//all white
-	/*	if(Center_white_line<40 && Left_white_line<40 && Right_white_line<40 && f1==0)
-		{
-			//right if white black white return
-			//left  if white black white return
-			lcd_print (1,1,1,1);
-			buzzer_on();
-			gadbad();
-				
-			//while(1);
-			f1=1;
-			//lcd_print (1,1,i,2);
-		}
-		
-		*/
-		//object detected in front
-		if(Sharp_Sensor>110)
-		{
-			stop();
-		
-			pickup();
-		
-			right();
-			right();
-		}
-		//left black
-		if(Center_white_line<40 && Left_white_line>40 && Right_white_line<40)	
-		{
-			buzzer_on();
-			correct(0x02);
-		}
-		//right black
-		if(Center_white_line<40 && Left_white_line<40 && Right_white_line>40)
-		{
-			buzzer_on();
-			correct(0x04);
-		}
-		//all black 
-		if(Center_white_line>40 && Left_white_line>40 && Right_white_line>40)
-		{
-			stop();
-			right();
-			right();
-		}
-			
-	}
+		set_color();
+	}	
 }
